cd93a1a416d820b2990b15d55fdb2737b0978e3e
jeremy <jeremy@jeremyzjl>
2020-12-18 09:09:41 +0800

    修改login页面

--- webadmins/static/lib/util.js
+++ webadmins/static/lib/util.js
@@ -18,32 +18,32 @@ function modalalertdemo(msg, interval=5000){
     $.Huimodalalert(msg ,interval)
 }
 
-
 function login_acquire(){
     var cookie = document.cookie;
-    console.log('login_acquire - cookie - ', cookie, 'login_acquire - cookieType - ', typeof cookie);
+    console.log('login_acquire - cookie : ', cookie, 'login_acquire - cookieType : ', typeof cookie);
 
-    var re = /login=true/i;
-    var login = cookie.search(re);
-    console.log('login_acquire - login - ', login, 'login - type - ', typeof login, 're - type - ', typeof re);
+    var re = /login=true/i;  // 正则表达式
+    var login = cookie.search(re);  // 正则匹配，计算出cookie中是否有'login=true'
+    /* cookie这个变量是string，所以可以使用search()方法
+    search() 方法用于检索字符串中指定的子字符串，或检索与正则表达式相匹配的子字符串。
+    如果没有找到任何匹配的子串，则返回 -1。 */
+    // console.log('login_acquire - login : ', login);
 
-    if (login === -1){
+    if (login === -1){  // 如果上面没有匹配到 'login=true', 则表示未登录，返回登录页面
         window.open("/static/login.html", '_self')
     }
 }
 
-
-
-function format_timestamp(timestamp) {
-        let date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
-        let Y = date.getFullYear() + '-';
-        let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-        let D = date.getDate() + ' ';
-        let h = date.getHours() + ':';
-        let m = (date.getMinutes() < 10 ? '0'  + date.getMinutes() : date.getMinutes()) + ':';
-        let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
-        return Y+M+D+h+m+s;
-    }
+function format_timestamp(timestamp) {  // 日期格式化
+    let date = new Date(timestamp * 1000);  //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+    let Y = date.getFullYear() + '-';
+    let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    let D = date.getDate() + ' ';
+    let h = date.getHours() + ':';
+    let m = (date.getMinutes() < 10 ? '0'  + date.getMinutes() : date.getMinutes()) + ':';
+    let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+    return Y+M+D+h+m+s;
+}
     
 function ajaxGet(url){
 //不带参数的 ajaxget请求
